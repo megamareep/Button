7544d7e383234f46c18f715640e701fadbff738c
Michael Adams <github@michaeladams.org>
2020-07-11 18:35:47 +1200

    Fix compile warning

--- Button.cpp
+++ Button.cpp
@@ -11,8 +11,8 @@ Button::Button(uint8_t pin, uint16_t debounce_ms)
 :  _pin(pin)
 ,  _delay(debounce_ms)
 ,  _state(HIGH)
-,  _has_changed(false)
 ,  _ignore_until(0)
+,  _has_changed(false)
 {
 }
 
