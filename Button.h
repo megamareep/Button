3e8b75cb25b3be739ce8e4e21fc42b3f506fc8ff
megamareep <delltom89@gmail.com>
2020-07-31 14:36:53 +0100

    Update Button.h

--- Button.h
+++ Button.h
@@ -20,6 +20,6 @@ class Button
 		uint32_t m_ignore_until;//4 bytes
 		const uint16_t m_delay;//2 bytes
 		const uint8_t  m_pin;//1 byte
-		uint8_t m_state : 1; //1
-		uint8_t m_has_changed : 1; //1
+		uint8_t m_state : 1; 
+		uint8_t m_has_changed : 1; 
 };
