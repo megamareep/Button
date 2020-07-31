5948dd5e3ec2e11de0b8353004e3fc0562cdad2f
megamareep <delltom89@gmail.com>
2020-07-31 14:35:44 +0100

    Updated Interface

--- Button.cpp
+++ Button.cpp
@@ -1,75 +1,51 @@
-/*
-	Button - a small library for Arduino to handle button debouncing
-	
-	MIT licensed.
-*/
-
 #include "Button.h"
-#include <Arduino.h>
 
 Button::Button(uint8_t pin, uint16_t debounce_ms)
-:  _ignore_until(0)
-,  _delay(debounce_ms)
-,  _pin(pin)
-,  _state(HIGH)
-,  _has_changed(false)
+:  m_ignore_until(0)
+,  m_delay(debounce_ms)
+,  m_pin(pin)
+,  m_state(State::eReleased)
+,  m_has_changed(false)
 {
+	pinMode(m_pin, INPUT_PULLUP);
 }
 
-void Button::begin()
+Button::State Button::read()
 {
-	pinMode(_pin, INPUT_PULLUP);
-}
-
-// 
-// public methods
-// 
-
-bool Button::read()
-{
-	// ignore pin changes until after this delay time
-	if (_ignore_until > millis())
+	m_has_changed = false;
+	if (m_ignore_until > millis())
 	{
 		// ignore any changes during this period
 	}
 	
 	// pin has changed 
-	else if (digitalRead(_pin) != _state)
+	else if (digitalRead(m_pin) != m_state)
 	{
-		_ignore_until = millis() + _delay;
-		_state = !_state;
-		_has_changed = true;
+		m_ignore_until = millis() + m_delay;
+		m_state = !m_state;
+		m_has_changed = true;
 	}
 	
-	return _state;
+	return static_cast<Button::State>(m_state);
 }
 
-// has the button been toggled from on -> off, or vice versa
-bool Button::toggled()
+
+bool Button::uniquePressed()
 {
-	read();
-	return has_changed();
+	return (pressed() && m_has_changed);
 }
 
-// mostly internal, tells you if a button has changed after calling the read() function
-bool Button::has_changed()
+bool Button::uniqueReleased()
 {
-	if (_has_changed)
-	{
-		_has_changed = false;
-		return true;
-	}
-	return false;
+	return (released() && m_has_changed);
 }
 
-// has the button gone from off -> on
 bool Button::pressed()
 {
-	return (read() == PRESSED && has_changed());
+	return (read() == State::ePressed);
 }
 
-// has the button gone from on -> off
 bool Button::released()
 {
-	return (read() == RELEASED && has_changed());
+	return (read() == State::eReleased);
 }
